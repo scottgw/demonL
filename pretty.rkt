1f3da7c33cdbd2395deeb7a04a2a5ad9670710fb
Scott West <scott.west@inf.ethz.ch>
2011-06-01 13:57:14 +0200

    Adding a pretty printing racket file.
    This allows the generated yices lisp file to be pretty printed and be
    more readable.

---
+++ pretty.rkt
@@ -0,0 +1,21 @@
+#!/usr/bin/racket
+
+#lang racket
+
+(require racket/pretty)
+
+(define arg1 (vector-ref (current-command-line-arguments) 0))
+(define infile (open-input-file arg1))
+(define outfile (open-output-file (string-append "pretty_" arg1) 
+                              #:exists 
+                              'truncate))
+
+(define (pretty-it in out)
+    (let ((str (read-line in)))
+      (if (eof-object? str)
+          (begin (close-output-port out)
+                 (close-input-port in))
+          (begin (pretty-display (read (open-input-string str)) out)
+                 (pretty-it in out)))))
+
+(pretty-it infile outfile)
